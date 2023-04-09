2c4a7af11e51a4ff98198c2f60371f06ea9558b3
Grégory Marini <greg.marini@hotmail.fr>
2023-04-09 18:18:37 +0200

    end day commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Fragment } from 'react';
 // import Animals from './pages/Animals.pages';
 // import SearchImages from './pages/SearchImages.pages';
 import BooksApp from './pages/BooksApp';
-import './general.css';
 
 function App() {
   return (
