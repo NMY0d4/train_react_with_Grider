635736f783f554f6b88a6548810952858f743176
Grégory Marini <greg.marini@hotmail.fr>
2023-04-11 12:17:30 +0200

    introducing use context

--- src/components/BooksApp/BookList.js
+++ src/components/BooksApp/BookList.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import { useContext } from 'react';
+import BooksContext from '../../context/books.context';
 import BookShow from './BookShow';
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const { count, incrementCount } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => (
     <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
   ));
-  return <div className='book-list'>{renderedBooks}</div>;
+  return (
+    <div className='book-list'>
+      <div>
+        <p>{count}</p>
+        <button onClick={incrementCount}>click</button>
+      </div>
+      {renderedBooks}
+    </div>
+  );
 };
 
 export default BookList;
