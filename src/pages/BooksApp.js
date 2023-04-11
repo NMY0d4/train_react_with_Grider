1083ed26679a5e137e239333bb0ada7db8a6bfc7
Grégory Marini <greg.marini@hotmail.fr>
2023-04-11 10:34:59 +0200

    updating and deleting with axios

--- src/pages/BooksApp.js
+++ src/pages/BooksApp.js
@@ -16,18 +16,24 @@ const BooksApp = () => {
     fetchBooks();
   }, []);
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     setBooks(
       books.map((book) => {
         if (book.id === id) {
-          return { ...book, title: newTitle };
+          return { ...book, ...response.data };
         }
         return book;
       })
     );
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     setBooks(books.filter((book) => book.id !== id));
   };
 
