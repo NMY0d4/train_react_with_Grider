0c068807b2fdaa4e80b1faa934dbe7abb32f0f88
Grégory Marini <greg.marini@hotmail.fr>
2023-04-14 12:11:46 +0200

    start build fucntionnalities in Dropdown component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { Fragment } from 'react';
 import Dropdown from './components/ui/Dropdown/Dropdown';
 
 function App() {
+  const options = [
+    { label: 'red', value: 'red' },
+    { label: 'green', value: 'green' },
+    { label: 'blue', value: 'blue' },
+  ];
+
   return (
     <Fragment>
-      <Dropdown />
+      <Dropdown options={options} />
     </Fragment>
   );
 }
--- src/components/ui/Dropdown/Dropdown.js
+++ src/components/ui/Dropdown/Dropdown.js
@@ -1,5 +1,30 @@
-function Dropdown() {
-  return <div>Dropdown</div>;
+import { useState } from 'react';
+
+function Dropdown({ options, handleSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen((currentIsOpen) => !currentIsOpen);
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    console.log(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className='noselect' onClick={handleOpen}>
+        Select...
+      </div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
