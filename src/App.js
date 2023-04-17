de27795290e6b4d337421984c5316b3414c3e244
Grégory Marini <greg.marini@hotmail.fr>
2023-04-17 11:31:35 +0200

    refactoring(create DropdownPage)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import { Fragment, useState } from 'react';
-import Dropdown from './components/ui/Dropdown/Dropdown';
+import { Fragment } from 'react';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
-  const [selection, setSelection] = useState(null);
-  const options = [
-    { label: "it's red", value: 'red' },
-    { label: "it's green", value: 'green' },
-    { label: "it's blue", value: 'blue' },
-  ];
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
   return (
     <Fragment>
-      <div className='flex'>
-        <Dropdown options={options} value={selection} onChange={handleSelect} />
-      </div>
+      <DropdownPage />
     </Fragment>
   );
 }
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Dropdown from '../components/ui/Dropdown/Dropdown';
+
+function DropdownPage() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: "it's red", value: 'red' },
+    { label: "it's green", value: 'green' },
+    { label: "it's blue", value: 'blue' },
+  ];
+
+  return (
+    <div className='flex'>
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
+}
+
+export default DropdownPage;
