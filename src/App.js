9acb28401cafdbaadd8bf9fe18e99de539f1e8fe
Grégory Marini <greg.marini@hotmail.fr>
2023-04-17 15:40:29 +0200

    add navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Fragment } from 'react';
+import Link from './components/navigation/Link';
+import Route from './components/navigation/Route';
 import DropdownPage from './pages/DropdownPage';
+import AccordionPage from './pages/Accordion.pages';
 
 function App() {
   return (
-    <Fragment>
-      <DropdownPage />
-    </Fragment>
+    <div>
+      <Link to='/accordion'>Go to accordion</Link>
+      <Link to='/dropdown'>Go to dropdown</Link>
+      <Route path="/accordion">
+        <AccordionPage/>
+      </Route>
+      <Route path="/dropdown">
+        <DropdownPage/>
+      </Route>
+      
+    </div>
   );
 }
 
