5eea4abd2b79cc5089151efb65de798c9c66dcc4
Grégory Marini <greg.marini@hotmail.fr>
2023-04-20 18:32:43 +0200

    update CounterPage

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,6 +4,7 @@ import Panel from '../components/ui/Panel';
 
 export default function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -13,20 +14,34 @@ export default function CounterPage({ initialCount }) {
     setCount(count - 1);
   };
 
+  const handleChange = (e) => {
+    const value = +e.target.value;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
+
   return (
     <Panel className='m-3'>
       <h1 className='text-lg'>count is {count}</h1>
-      <div>
+      <div className='flex flex-row gap-3'>
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </div>
-      <form action=''>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
+          value={valueToAdd || ''}
+          onChange={handleChange}
           type='number'
           className='p-1 m-3 bg-gray-50 border border-gray-300'
         />
-        <Button>Add -t!</Button>
+        <Button>Add it!</Button>
       </form>
     </Panel>
   );
